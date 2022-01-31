6764a1c2f6132ff7f5f1b89a559b1fa46a93ff8c
Miles Watson <contact@mileswatson.net>
2022-01-31 11:20:28 +0000

    Replaced top down implementation with bottom-up.

--- dynamic_programming/src/bin/rods.rs
+++ dynamic_programming/src/bin/rods.rs
@@ -2,7 +2,7 @@ use dynamic_programming::rods::max_price;
 
 fn main() {
     let prices = vec![0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
-    let size = 10;
+    let size = 8;
     let price = max_price(size, &prices);
     println!("Max price of {size}: {price}")
 }
--- dynamic_programming/src/rods.rs
+++ dynamic_programming/src/rods.rs
@@ -1,32 +1,19 @@
-use std::collections::HashMap;
-
-fn max_price_memo(rod: u32, prices: &[u32], memo: &mut HashMap<u32, u32>) -> u32 {
-    if let Some(&price) = memo.get(&rod) {
-        return price;
-    }
-    let max = prices
-        .iter()
-        .enumerate()
-        .filter_map(|(l, p)| {
-            if 0 < l && (l as u32) < rod {
-                Some((l as u32, p))
-            } else {
-                None
-            }
-        })
-        .map(|(l, p)| p + max_price_memo(rod - l, prices, memo))
-        .max()
-        .unwrap_or(0);
-    if let Some(&price) = prices.get(rod as usize) {
-        if price > max {
-            memo.insert(rod, price);
-            return price;
-        }
-    }
-    memo.insert(rod, max);
-    max
-}
-
 pub fn max_price(rod: u32, prices: &[u32]) -> u32 {
-    max_price_memo(rod, prices, &mut HashMap::new())
+    let mut max_prices = vec![0];
+    for i in 1..=rod {
+        let max = prices
+            .iter()
+            .enumerate()
+            .filter_map(|(l, p)| {
+                if 0 < l && (l as u32) <= i {
+                    Some(p + max_prices[i as usize - l])
+                } else {
+                    None
+                }
+            })
+            .max()
+            .unwrap();
+        max_prices.push(max);
+    }
+    *max_prices.last().unwrap()
 }
